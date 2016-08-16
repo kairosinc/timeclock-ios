ae31491c374fb9ee8a2c245f983339da757588cf
Tom Hutchinson <tomhut101@gmail.com>
2016-08-16 09:30:40 +0100

    Add view controllers to composite view as properties

--- Timeclock/TimeClockViewController.swift
+++ Timeclock/TimeClockViewController.swift
@@ -10,26 +10,48 @@ import UIKit
 
 class TimeClockViewController: UIViewController {
 
+    //MARK: IBOutlet
+    @IBOutlet weak var clockOptionsView: UIView!
+    @IBOutlet weak var idleView: UIView!
+    @IBOutlet weak var captureView: UIView!
+    
+    //MARK: Properties
+    var clockOptionsViewController: ClockOptionsViewController?
+    var idleViewController: IdleViewController?
+    var captureViewController: CaptureViewController?
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        guard let segueIdentifier = segue.identifier else { return }
+        
+        switch segueIdentifier {
+        case "embedIdleViewController":
+            guard let destination = segue.destinationViewController as? IdleViewController else { break }
+            idleViewController = destination
+            break
+        case "embedCaptureViewController":
+            guard let destination = segue.destinationViewController as? CaptureViewController else { break }
+            captureViewController = destination
+            break
+            
+        case "embedClockOptionsViewController":
+            guard let destination = segue.destinationViewController as? ClockOptionsViewController else { break }
+            clockOptionsViewController = destination
+            break
+            
+        default:
+            break
+        }
     }
-    */
 
 }
